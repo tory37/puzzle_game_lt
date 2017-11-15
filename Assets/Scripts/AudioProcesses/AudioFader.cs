32666b8d28e411509efa0724e501ac846b4d270c
Land00m <landonviator@gmail.com>
2017-11-15 14:18:46 -0600

    commit

---
+++ Assets/Scripts/AudioProcesses/AudioFader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFader : MonoBehaviour
+{
+    //Each level/room gets an audiosource with that level/rooms music
+    //attach this to each level/room to fade in and out between each room
+    //works when player enters and exits each level/room
+
+	private AudioSource audiosource;
+
+	void Start()
+	{
+		audiosource = GetComponent<AudioSource>();
+
+        //makes sure AudioSource is off to begin
+		audiosource.enabled = false;
+
+	}
+
+	IEnumerator OnTriggerEnter2D(Collider2D other)
+	{
+        //Turn on AudioSource
+		audiosource.enabled = true;
+
+        //Interupts OnTriggerEnter2D to fade in
+		yield return StartCoroutine(FadeIn());
+	}
+
+	IEnumerator OnTriggerExit2D(Collider2D other)
+	{
+        //Fade out first before AudioSource is turned off
+		yield return StartCoroutine(FadeOut());
+
+        //Turn off AudioSource
+		audiosource.enabled = false;
+
+	}
+
+	IEnumerator FadeIn()
+	{
+		while (audiosource.volume < 1)
+		{
+			audiosource.volume += 1 * Time.deltaTime * .5f;
+
+			yield return null;
+		}
+	}
+
+	IEnumerator FadeOut()
+	{
+		while (audiosource.volume > 0)
+		{
+			audiosource.volume -= 1 * Time.deltaTime / 2;
+
+			yield return null;
+		}
+	}
+}
