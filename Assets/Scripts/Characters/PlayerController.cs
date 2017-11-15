4eb02ce8e6724bf13801686a3774db200b6fe9a9
Tory Hebert <tory37@gmail.com>
2017-11-14 21:37:08 -0600

    simple player jumping

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -10,19 +10,40 @@ public class PlayerController : MonoBehaviour {
     private PlayerMovement playerMovement;
 
     [SerializeField]
-    private Rigidbody rigidBody;
+    private Rigidbody playerRigidBody;
+
+    private Transform playerTransform;
 
     #endregion
 
     // Use this for initialization
     void Start () {
-		
+        playerTransform = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        MovePlayer();
+        RotatePlayer();
+        JumpPlayer();
+	}
+
+    private void MovePlayer() {
         float forwardInput = Input.GetAxis("Vertical");
         float strafeInput = Input.GetAxis("Horizontal");
-        playerMovement.Move(rigidBody, forwardInput, strafeInput);
-	}
+        playerMovement.Move(playerTransform, playerRigidBody, forwardInput, strafeInput);
+    }
+
+    private void RotatePlayer() {
+        float horizontalInput = Input.GetAxis("Mouse X");
+        playerMovement.Rotate(playerRigidBody, horizontalInput);
+    }
+
+    private void JumpPlayer() {
+        bool jumpInput = Input.GetButtonDown("Jump");
+        if (jumpInput)
+        {
+            playerMovement.Jump(playerRigidBody);
+        }
+    }
 }
--- Assets/Scripts/Characters/PlayerMovement.cs
+++ Assets/Scripts/Characters/PlayerMovement.cs
@@ -2,21 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     #region Variables
-
+    [Header("Move")]
     [SerializeField]
     private float forwardSpeed = 5.0f;
     [SerializeField]
     private float strafeSpeed = 5.0f;
 
+    [Header("Rotate")]
+    [SerializeField]
+    private float rotateSpeed = 100.0f;
+
+    [Header("Jump")]
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+
     #endregion
 
-    public void Move(Rigidbody rigidBody, float forwardInput, float strafeInput) {
+    public void Move(Transform transform, Rigidbody rigidBody, float forwardInput, float strafeInput) {
         float deltaForward = forwardInput * forwardSpeed * Time.deltaTime;
         float deltaStrafe = strafeInput * strafeSpeed * Time.deltaTime;
-        Vector3 deltaMovement = new Vector3(deltaStrafe, 0.0f, deltaForward);
-        rigidBody.MovePosition(rigidBody.position + deltaMovement);
+
+        Vector3 deltaForwardVector = deltaForward * transform.forward;
+        Vector3 deltaStrafeVector = deltaStrafe * transform.right;
+        Vector3 deltaMovementVector = deltaForwardVector + deltaStrafeVector;
+
+        rigidBody.MovePosition(rigidBody.position + deltaMovementVector);
+    }
+
+    public void Rotate(Rigidbody rigidBody, float horizontalInput) {
+        float deltaHorizontal = horizontalInput * rotateSpeed * Time.deltaTime;
+        Vector3 deltaVectorRotation = new Vector3(0.0f, deltaHorizontal, 0.0f);
+        Quaternion deltaQuaternionRotation = Quaternion.Euler(deltaVectorRotation);
+        rigidBody.MoveRotation(rigidBody.rotation * deltaQuaternionRotation);
+    }
+
+    public void Jump(Rigidbody rigidBody) {
+        Vector3 gravity = Physics.gravity;
+        rigidBody.velocity = -gravity * jumpHeight;
     }
 }
