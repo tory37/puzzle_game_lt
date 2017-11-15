6e67dd0a1f405ee52f3e2d5ac43b89f19590c341
Tory Hebert <tory37@gmail.com>
2017-11-14 20:34:00 -0600

    Basic player movement forward and strafing

---
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    #region Components
+
+    [SerializeField]
+    private PlayerMovement playerMovement;
+
+    [SerializeField]
+    private Rigidbody rigidBody;
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float forwardInput = Input.GetAxis("Vertical");
+        float strafeInput = Input.GetAxis("Horizontal");
+        playerMovement.Move(rigidBody, forwardInput, strafeInput);
+	}
+}
--- Assets/Scripts/Characters/PlayerMovement.cs
+++ Assets/Scripts/Characters/PlayerMovement.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #region Variables
+
+    [SerializeField]
+    private float forwardSpeed = 5.0f;
+    [SerializeField]
+    private float strafeSpeed = 5.0f;
+
+    #endregion
+
+    public void Move(Rigidbody rigidBody, float forwardInput, float strafeInput) {
+        float deltaForward = forwardInput * forwardSpeed * Time.deltaTime;
+        float deltaStrafe = strafeInput * strafeSpeed * Time.deltaTime;
+        Vector3 deltaMovement = new Vector3(deltaStrafe, 0.0f, deltaForward);
+        rigidBody.MovePosition(rigidBody.position + deltaMovement);
+    }
 }
