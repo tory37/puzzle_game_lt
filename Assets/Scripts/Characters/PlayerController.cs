fda2957c4aef9004f12504507dddc354dde84537
Tory Hebert <tory37@gmail.com>
2017-11-15 09:02:12 -0600

    Worked More on Jumping
    - Added a Multitag system
    - Added checks to see if touching ground to allow for jumping

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     #region Components
-
+    [Header("Components")]
     [SerializeField]
     private PlayerMovement playerMovement;
 
@@ -16,34 +17,73 @@ public class PlayerController : MonoBehaviour {
 
     #endregion
 
+    #region Variables 
+    [Header("Jump Raycast")]
+    [SerializeField]
+    private Vector3 jumpRaycastOffset;
+    [SerializeField]
+    private float jumpRaycastDistance;
+    [SerializeField]
+    private LayerMask jumpRaycastLayerMask;
+
+    #endregion
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         playerTransform = GetComponent<Transform>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         MovePlayer();
         RotatePlayer();
         JumpPlayer();
-	}
+    }
 
-    private void MovePlayer() {
+    private void MovePlayer()
+    {
         float forwardInput = Input.GetAxis("Vertical");
         float strafeInput = Input.GetAxis("Horizontal");
         playerMovement.Move(playerTransform, playerRigidBody, forwardInput, strafeInput);
     }
 
-    private void RotatePlayer() {
+    private void RotatePlayer()
+    {
         float horizontalInput = Input.GetAxis("Mouse X");
         playerMovement.Rotate(playerRigidBody, horizontalInput);
     }
 
-    private void JumpPlayer() {
-        bool jumpInput = Input.GetButtonDown("Jump");
-        if (jumpInput)
+    private void JumpPlayer()
+    {
+        #if UNITY_EDITOR
+        Debug.DrawRay(playerTransform.position + jumpRaycastOffset, -playerTransform.up * jumpRaycastDistance, Color.red);
+        #endif
+
+        if (CanJump())
         {
-            playerMovement.Jump(playerRigidBody);
+            bool jumpInput = Input.GetButtonDown("Jump");
+            if (jumpInput)
+            {
+                playerMovement.Jump(playerRigidBody);
+            }
         }
+    }
+
+    private bool CanJump() {
+        RaycastHit raycastHit;
+        if (Physics.Raycast(playerTransform.position + jumpRaycastOffset, -playerTransform.up, out raycastHit, jumpRaycastDistance, jumpRaycastLayerMask )) {
+            Collider otherCollider = raycastHit.collider;
+            MultiTagSystem otherTags = otherCollider.GetComponent<MultiTagSystem>();
+            if (otherTags)
+            {
+                if (otherTags.HasTag(MultiTag.Jumpable))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
---
+++ Assets/Scripts/Custom_Components/MultiTagSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MultiTag {
+    Jumpable
+}
+
+public class MultiTagSystem : MonoBehaviour {
+
+    [SerializeField]
+    private List<MultiTag> tags;
+
+    public bool HasTag(MultiTag tag) {
+        if (tags.Contains(tag))
+            return true;
+        return false;
+    }
+}
